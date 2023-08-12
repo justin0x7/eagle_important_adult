1ec9c7f8698c08323f7a99691de3698934891f91
Talent0407 <imasnpsorrels5@gmail.com>
2023-08-13 02:19:36 +0800

    complete

--- src/core/constants/base.const.ts
+++ src/core/constants/base.const.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || "";
+export const API_URL = process.env.REACT_APP_API_URL || "https://survey.eagle.productions";
 export const QUESTIONNAIRES_URL = process.env.REACT_APP_QUESTIONNAIRES_URL || "";
 export const QUESTIONNAIRES_URL1 = process.env.REACT_APP_QUESTIONNAIRES_URL1 || "";
